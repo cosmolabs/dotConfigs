002369ee476a4205d677e525eb087acb7ced6bc4
Ganesh Kuramsetti <ganeshhkuramsetti@gmail.com>
2020-12-02 06:31:52 -0500

    Added dependency for libwebkit, and downloading citrix via curl

--- citrixReceiverForDebian.sh
+++ citrixReceiverForDebian.sh
@@ -9,13 +9,12 @@
 #1. Download icaClient receiver from citrix website and keep it in Downloads folder.
 #2. Run the shell script as normal user and use sh to run the script instead of "./"
 #3. Enter the sudo password when promted.
-#Modified : Aug 08 2020 - Added libjpeg dependency for libwebkit
+#Modified : Dec 02 2020. Added dependency libegl1-mesa and download url for citrix receiver.
 #=========================================================================================================================
 
 #System paths && variables
 tmpPath=/tmp
 tmpDownloadFolder=$(echo ${0} | awk '{ print $1}' FS=".")
-icaClientDownloadedFilePath=~/Downloads/icaclient*
 
 #Creating temporary folder for downloads and installations.
 #echo "\nMoving the execution to temp directory ......."
@@ -27,14 +26,16 @@ curl -O http://ftp.br.debian.org/debian/pool/main/i/icu/libicu57_57.1-6+deb9u4_a
 curl -O http://ftp.br.debian.org/debian/pool/main/w/webkitgtk/libjavascriptcoregtk-1.0-0_2.4.11-3_amd64.deb
 curl -O http://ftp.br.debian.org/debian/pool/main/w/webkitgtk/libwebkitgtk-1.0-0_2.4.11-3_amd64.deb
 curl -O http://ftp.br.debian.org/debian/pool/main/libj/libjpeg-turbo/libjpeg62-turbo_1.5.2-2+b1_amd64.deb
+curl -O http://ftp.br.debian.org/debian/pool/main/m/mesa/libegl1-mesa_18.3.6-2+deb10u1_amd64.deb
+curl -O "https://downloads.citrix.com/14822/icaclient_13.10.0.20_amd64.deb?__gda__=1606908542_be5a627bdce11743325f6eb2853dd416"
 
 #Moving ica client package to execution folder
-echo "\nCopying icaclient deb package to a tmp directory......"
-sudo cp ${icaClientDownloadedFilePath} $(pwd)
+#echo "\nCopying icaclient deb package to a tmp directory......"
+#sudo cp ${icaClientDownloadedFilePath} $(pwd)
 
 #installing all the deb packages as root and answering yes for installation
 echo "\nInstallation started ....."
-sudo apt install -y ./libicu57_57* && sudo apt install -y ./libjavascriptcoregtk* && sudo apt install -y ./libjpeg62* && sudo apt install -y ./libwebkitgtk* && sudo apt install -y ./icaclient*
+sudo apt install -y ./libicu57_57* && sudo apt install -y ./libjavascriptcoregtk* && sudo apt install -y ./libjpeg62* && sudo apt install -y ./libegl1-mesa* && sudo apt install -y ./libwebkitgtk* && sudo apt install -y ./icaclient*
 
 #Copying certificates to make citrix work.
 echo "\nCopying required certificates"
