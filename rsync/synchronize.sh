732c16e2476aba03ad647efc2fc11e75956992f0
Ganesh Kuramsetti <ganeshhkuramsetti@gmail.com>
2021-06-10 21:07:48 -0300

    Updated synchronize script, adding additional functionality for deletion of files in destination directory.

--- rsync/synchronize.sh
+++ rsync/synchronize.sh
@@ -1,22 +1,44 @@
 #!/usr/bin/bash
 
+# A bash script to sync two directories.
+# updated on - June 10th, 2021.
 
 echo -e  "\n"
 read -p "Enter the full path of source directory: " sourceDirectory
 echo -e "\n"
 read -p "Enter the full path of destination directory: " destinationDirectory
+
 echo -e "\nSource Direcory is: ${sourceDirectory}"
 echo "Destination Directory is: ${destinationDirectory}"
 
 echo -e "\nExecuting dry run ......"
 
-rsync -avPuhn ${sourceDirectory} ${destinationDirectory}
+# delelet option will delete the files that are not in source directory.
+rsync -avPuhn --delete ${sourceDirectory} ${destinationDirectory}
 
-echo -e "\n"
-read -p "Dry run completed. Do you want to sync the directories?(Y or N): " executeActualRun
+echo -e "\nDry run completed. If there is any file that's in source and not in destination was marked as delete."
+read -p "Do you want to delete the marked files? (Y or N) " deleteFilesinDest
 
-if [ ${executeActualRun} == "Y" ]
+# Check for deletion of files in destination.
+# If not comfortable with deletion, opt "N"
+if [ ${deleteFilesinDest} == "Y" ]
 then
-	rsync -avPu ${sourceDirectory} ${destinationDirectory}
-	echo -e "\nSynced source and destination directoires."
+    echo -e "\n"
+    read -p "Do you want to sync the directories? (Y or N): " executeActualRun
+
+    if [ ${executeActualRun} == "Y" ]
+    then
+	    rsync -avPhu --delete ${sourceDirectory} ${destinationDirectory}
+    	echo -e "\nSynced source and destination directoires."
+    fi
+elif [ ${deleteFilesinDest} == "N" ]
+then
+    echo -e "\n"
+    read -p "Do you want to sync the directories? (Y or N): " executeActualRun
+
+    if [ ${executeActualRun} == "Y" ]
+    then
+	    rsync -avPhu ${sourceDirectory} ${destinationDirectory}
+    	echo -e "\nSynced source and destination directoires."
+    fi
 fi
